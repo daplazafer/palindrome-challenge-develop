778066e8cf389e758eae520eca936ebc7105ecc5
dpf <dpf220691@gmail.com>
2019-09-03 06:37:40 +0200

    Refactor

--- src/main/java/com/ing/mwchapter/services/impl/PalindromeServiceImpl.java
+++ src/main/java/com/ing/mwchapter/services/impl/PalindromeServiceImpl.java
@@ -14,13 +14,13 @@ public class PalindromeServiceImpl implements IPalindromeService {
     @Override
     public Optional<String> highestValuePalindrome(String number, int maxAllowedChanges) {
         AtomicInteger remainingChanges = new AtomicInteger(maxAllowedChanges);
-        List<Integer> mirrors = IntStream.range(0, number.length() / 2).boxed()
+        List<Integer> costsToBe9 = IntStream.range(0, number.length() / 2).boxed()
                 .map(index -> getRemainingCostTo9(number, index, remainingChanges))
                 .collect(Collectors.toList());
 
         return remainingChanges.get() < 0
                 ? Optional.empty()
-                : buildHighestPalindrome(number, mirrors, remainingChanges);
+                : Optional.of(buildHighestPalindrome(number, costsToBe9, remainingChanges));
     }
 
     private static int getRemainingCostTo9(String number, int index, AtomicInteger remainingChanges) {
@@ -34,14 +34,14 @@ public class PalindromeServiceImpl implements IPalindromeService {
         return Math.min(uniqueDigitsWith9 - ((uniqueDigitsWith9 + mirrorCost) % 2), 2) - mirrorCost;
     }
 
-    private static Optional<String> buildHighestPalindrome(String number, List<Integer> mirrors, AtomicInteger remainingChanges) {
+    private static String buildHighestPalindrome(String number, List<Integer> costsToBe9, AtomicInteger remainingChanges) {
         String leftSide = IntStream.range(0, number.length() / 2).boxed()
-                .map(index -> remainingChanges.get() - mirrors.get(index) < 0
+                .map(index -> remainingChanges.get() - costsToBe9.get(index) < 0
                         ? String.valueOf(Math.max(left(number, index), right(number, index)))
-                        : updateChangesAndGet9(remainingChanges, mirrors.get(index)))
+                        : updateChangesAndGet9(remainingChanges, costsToBe9.get(index)))
                 .collect(Collectors.joining(""));
 
-        return Optional.of(buildPalindrome(number, leftSide, remainingChanges.get()));
+        return buildPalindrome(number, leftSide, remainingChanges.get());
     }
 
     private static String updateChangesAndGet9(AtomicInteger remainingChanges, int costToBe9) {
